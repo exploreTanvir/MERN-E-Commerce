4e8ea7032fdbb94779701236ab9426cbd18da924
Tanvir_Hossen <tanvirhossen853@gmail.com>
2023-12-21 18:26:03 +0600

    E-commerce app

--- server/src/server.js
+++ server/src/server.js
@@ -1,18 +1,37 @@
 const express=require("express")
 const morgan = require('morgan')
+const bodyParser=require("body-parser")
 const app=express()
 app.use(morgan("dev"))
 
 
 const isLoggedIn=(req,res,next)=>{
-    console.log("IsloggedIn middleWare")
-    next()
+    const loggedIn=false
+    if(loggedIn){
+        next()
+    }
+    else{
+        res.status(401).send("please login first")
+    }
+    
 }
 app.get("/test",(req,res)=>{
     res.status(200).send("api is working fine")
 })
 app.get("/api/user",isLoggedIn,(req,res)=>{
     res.status(200).send("user profile is return")
+})
+
+// client error handling
+app.use((req,res,next)=>{
+    res.status(404).send("Route not found")
+    next()
+
+})
+// server error handling
+app.use((err,req,res,next)=>{
+    res.status(500).send("Something broke")
+
 })
 
 
